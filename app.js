7917cb1d9af15119d4b6d80ee7e4d5fc052d96f6
Kristen Quinlan-Boyce <kqboyce@gmail.com>
2024-01-22 19:47:56 -0500

    working Rochambeau app
    moved playRound function into game function for 5 plays, adding a point for each win or no points for ties. Added event listener to dom content load to initiate prompt for game play. Wrote conditional for console logging to communicate game winner status.

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 // the computer is your opponent and needs to randomly select either rock paper or scissors
-
- function getComputerChoice() {
+document.addEventListener("DOMContentLoaded", function() {
+   game();
+  });
+let computerScore = 0;
+let playerScore = 0;
+  function game() {
+    for(i=0;i<5;i++) { 
+        playRound(playerSelection, computerSelection);     
+        if(computerScore==playerScore) {
+            console.log("tied that play");
+        }
+        console.log(`Computer choice: ${computerSelection}, Computer score: ${computerScore}, Player score ${playerScore}`)
+    }
+    if(computerScore > playerScore) {
+        console.log(`Commputer score: ${computerScore}. Computer Wins!`)
+    } else if(playerScore > computerScore) {
+        console.log(`Player score: ${playerScore}. Player Wins!`)
+    } else {
+        console.log("tied the round")
+    }
+} 
+function getComputerChoice() {
     let deciderNumber = Math.floor(Math.random() * (3));
     let rockPaperScissors = ["rock", "paper", "scissors"];
     let computerSelection = rockPaperScissors[deciderNumber];
     return computerSelection;
  }
 
- const computerSelection = getComputerChoice();
-console.log(computerSelection);
-function playRound(playerSelection, computerSelection) {
-        return playerSelection == computerSelection ? "tie!"
-          : playerSelection == "rock" && computerSelection == "scissors" ? "Player wins"
-          : playerSelection == "paper" && computerSelection == "scissors" ? "computer wins"
-          : playerSelection == "scissors" && computerSelection == "paper" ? "player"
-          : playerSelection == "scissors" && computerSelection == "rock" ? "Comp"
-          : playerSelection == "rock" && computerSelection == "paper" ? "Comp"
-          : playerSelection == "paper" && computerSelection == "rock" ? "player"
-        : "game over"
-}
+let computerSelection;
+let playerSelection;
 
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
+
+    return playerSelection == computerSelection ?  console.log("tied that play")
+    : playerSelection == "rock" && computerSelection == "scissors" ? playerScore++
+    : playerSelection == "scissors" && computerSelection == "paper" ?  playerScore++
+    : playerSelection == "paper" && computerSelection == "rock" ?  playerScore++
+    : playerSelection == "scissors" && computerSelection == "rock" ? computerScore++
+    : playerSelection == "rock" && computerSelection == "paper" ? computerScore++
+    : playerSelection == "paper" && computerSelection == "scissors" ? computerScore++
+    : "game over"     
+}
+    
 
 
