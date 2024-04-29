00d33f4b4dd60a06c8f8aedb408f1bb343d24e0e
Kristen Quinlan-Boyce <kqboyce@gmail.com>
2024-04-29 19:49:27 -0400

    refactored javascript to function outside of console
    added ui with html and css

--- app.js
+++ app.js
@@ -1,48 +1,94 @@
-// the computer is your opponent and needs to randomly select either rock paper or scissors
-document.addEventListener("DOMContentLoaded", function() {
-   game();
-  });
+// document.addEventListener("DOMContentLoaded", function() {
+ //call the game function upon dom content load
+    // game();
+
+ //initialize variables for global accessibility 
 let computerScore = 0;
 let playerScore = 0;
-  function game() {
-    for(i=0;i<5;i++) { 
-        playRound(playerSelection, computerSelection);     
-        if(computerScore==playerScore) {
-            console.log("tied that play");
-        }
-        console.log(`Computer choice: ${computerSelection}, Computer score: ${computerScore}, Player score ${playerScore}`)
-    }
-    if(computerScore > playerScore) {
-        console.log(`Commputer score: ${computerScore}. Computer Wins!`)
-    } else if(playerScore > computerScore) {
-        console.log(`Player score: ${playerScore}. Player Wins!`)
-    } else {
-        console.log("tied the round")
-    }
-} 
+const playerRock = document.querySelector("#rock");
+const playerScissors = document.querySelector("#scissors");
+const playerPaper = document.querySelector("#paper");
+const buttons = document.querySelectorAll("button");
+let compChoice = document.querySelector(".compChoice");
+let playChoice = document.querySelector(".playChoice");
+let player = document.querySelector('.playerScore');
+let computer = document.querySelector('.computerScore');
+let tie = document.querySelector('.tie');
+let playerSelection = 0;
+let computerSelection = 0;
+
+// call the play round function and execute 5 times, console log the results of each play and the winner of the round
+
+//   function game() {
+//     // for(i=0;i<5;i++) { 
+//         playRound(playerSelection, computerSelection);     
+//         if(computerScore==playerScore) {
+//             console.log("BOOM");
+//         }
+//         // console.log(`Computer score: ${computerScore}, Player score ${playerScore}`)
+//     }
+//     if(computerScore > playerScore) {
+//         console.log(`Computer score: ${computerScore}. Computer Wins!`)
+//     } else if(playerScore > computerScore) {
+//         console.log(`Player score: ${playerScore}. Player Wins!`)
+//     } else {
+//         console.log("tied the round")
+//     }
+// } 
+// the computer is your opponent and needs to randomly select either rock paper or scissors
 function getComputerChoice() {
     let deciderNumber = Math.floor(Math.random() * (3));
     let rockPaperScissors = ["rock", "paper", "scissors"];
-    let computerSelection = rockPaperScissors[deciderNumber];
+    computerSelection = rockPaperScissors[deciderNumber];
+    // console.log(computerSelection)
     return computerSelection;
  }
 
-let computerSelection;
-let playerSelection;
+// computer and person play a round of rock-paper-scissors aka Rochambeau with 7 possible outcomes per play including ties
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("rock, paper, or scissors?").toLowerCase();
-
-    return playerSelection == computerSelection ?  console.log("tied that play")
-    : playerSelection == "rock" && computerSelection == "scissors" ? playerScore++
-    : playerSelection == "scissors" && computerSelection == "paper" ?  playerScore++
-    : playerSelection == "paper" && computerSelection == "rock" ?  playerScore++
-    : playerSelection == "scissors" && computerSelection == "rock" ? computerScore++
-    : playerSelection == "rock" && computerSelection == "paper" ? computerScore++
-    : playerSelection == "paper" && computerSelection == "scissors" ? computerScore++
-    : "game over"     
-}
+function playRound() {
+        buttons.forEach(button => {
+            button.addEventListener("click", (e) => {
+                //get player and computer selection to evaluate the round
+                playerSelection = e.target.textContent;
+                computerSelection = getComputerChoice();
     
+                playerSelection == computerSelection ?  showMessage()
+                : playerSelection == "rock" && computerSelection == "scissors" ||
+                playerSelection == "scissors" && computerSelection == "paper" ||
+                playerSelection == "paper" && computerSelection == "rock" ?  playerScore++
+                : playerSelection == "scissors" && computerSelection == "rock" ||
+                playerSelection == "rock" && computerSelection == "paper" ||
+                playerSelection == "paper" && computerSelection == "scissors" ? computerScore++
+                : console.log('game over');
+   
+                console.log(`Computer score: ${computerScore}, Player score ${playerScore}`)   
+                compChoice.textContent = `Computer: ${computerSelection}`;
+                playChoice.textContent = `You: ${playerSelection}`;
+                player.textContent = `Score: ${playerScore}`;
+                computer.textContent = `Score: ${computerScore}`
+                
+                if(computerScore == 6 || playerScore == 6) {
+                    alert('Game OVER!');
+                    computerScore = 0;
+                    playerScore = 0
+                    player.textContent = `Score: ${playerScore}`;
+                    computer.textContent = `Score: ${computerScore}`
+                    return
+                }
+            }); 
+           
+        });          
+    }
+    //temporarily display tie message
+    function showMessage() {
+        tie.style.visibility = "visible";
+        setTimeout(() => {hideMessage()}, 2000);
+    }
+    function hideMessage() {
+        tie.style.visibility = "hidden";
+    }
+
+    playRound();
 
 
